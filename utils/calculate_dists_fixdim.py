38764025f8bf21fb8162f04f39abe972be858faf
Herman Kamper <kamperh@gmail.com>
2014-10-22 16:14:43 +0100

    Calculate distances using pdist

--- utils/calculate_dists_fixdim.py
+++ utils/calculate_dists_fixdim.py
@@ -8,6 +8,7 @@ Contact: h.kamper@sms.ed.ac.uk
 Date: 2014
 """
 
+from scipy.spatial.distance import pdist
 import argparse
 import datetime
 import sys
@@ -24,16 +25,16 @@ def check_argv():
     """Check the command line arguments."""
     parser = argparse.ArgumentParser(description=__doc__.strip().split("\n")[0], add_help=False)
     parser.add_argument(
-        "pairs_fn", help="a file of a list of the pairs of utterance IDs for which distances should be "
-        "calculated"
-        )
-    parser.add_argument(
         "features_fn", help="the file containing features; "
         "this should be in .npz numpy archive format"
         )
+    parser.add_argument("labels_fn", help="output labels file")
     parser.add_argument(
-        "distances_fn", help="the distances are written to this file "
-        "in the same order as which the pairs occur in `pairs_fn`"
+        "distances_fn", help="the distances are written to this file"
+        )
+    parser.add_argument(
+        "--binary_dists", dest="binary_dists", action="store_true",
+        help="write distances in float32 binary format (default is not to do this)"
         )
     if len(sys.argv) == 1:
         parser.print_help()
@@ -49,36 +50,36 @@ def main():
     args = check_argv()
     
     print datetime.datetime.now()
-    print "Reading pairs from:", args.pairs_fn
-    pairs = read_pairs(args.pairs_fn)
-    print "Reading features from:", features_fn
-    if args.input_fmt == "kaldi_txt":
-        ark = read_kaldi_ark(features_fn)
-    elif args.input_fmt == "npz":
-        ark = np.load(features_fn)
-        ark = dict(ark)
+    print "Reading features from:", args.features_fn
+    ark = np.load(args.features_fn)
+
+    print "Calculating distances"
+    X = []
+    labels = []
+    for label in ark:
+        labels.append(label)
+        X.append(ark[label])
+    X = np.array(X)
+    distances = pdist(X, metric="cosine")
 
 
-    sys.exit()
+    # print "Calculating distances"
+    # distances = np.zeros(len(pairs))
+    # for i_pair, pair in enumerate(pairs):
+    #     utt_id_1, utt_id_2 = pair
+    #     distances[i_pair] = cosine(ark[utt_id_1], ark[utt_id_2])
 
-
-    ark = np.load(args.vectors_npz_fn)
-    ark = dict(ark)
-    labels = sorted(ark.keys())
-    m = len(labels)
-    print m
-    print "Writing labels:", args.labels_fn
+    # Write to file
+    if args.binary_dists:
+        print "Writing distances to binary file:", args.distances_fn
+        np.asarray(distances, dtype=np.float32).tofile(args.distances_fn)
+    else:
+        print "Writing distances to text file:", args.distances_fn
+        np.asarray(distances, dtype=np.float32).tofile(args.distances_fn, "\n")
+        open(args.distances_fn, "a").write("\n")  # add final newline
+    print "Writing labels to file:", args.labels_fn
     open(args.labels_fn, "w").write("\n".join(labels) + "\n")
-
-    # # Generate distances for all possible pairs
-    # f = open(pairs_fn, "w")
-    # for i in xrange(0, m - 1):
-    #     for j in xrange(i + 1, m):
-    #         f.write(labels[i] + " " + labels[j] + "\n")
-    # f.close()
-    # print "Wrote pairs to file:", pairs_fn
-
-
+    print datetime.datetime.now()
 
 
 if __name__ == "__main__":
