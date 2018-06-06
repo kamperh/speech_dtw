907541d4956ffb184cd2273218cf0aa092080936
Herman Kamper <kamperh@gmail.com>
2018-06-06 08:43:24 +0200

    Updated samediff.py

--- utils/samediff.py
+++ utils/samediff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Functionality for performing same-different evaluation.
+Functions for performing same-different evaluation.
 
 Details are given in:
 - M. A. Carlin, S. Thomas, A. Jansen, and H. Hermansky, "Rapid evaluation of
@@ -9,13 +9,15 @@ Details are given in:
   2011.
 
 Author: Herman Kamper
-Contact: h.kamper@sms.ed.ac.uk
-Date: 2014
+Contact: kamperh@gmail.com
+Date: 2014, 2015, 2018
 """
 
+from __future__ import division
+from __future__ import print_function
+from datetime import datetime
 from scipy.spatial.distance import pdist
 import argparse
-import datetime
 import numpy as np
 import sys
 
@@ -24,7 +26,7 @@ import sys
 #                              SAMEDIFF FUNCTIONS                             #
 #-----------------------------------------------------------------------------#
 
-def average_precision(pos_distances, neg_distances, show_plot):
+def average_precision(pos_distances, neg_distances, show_plot=False):
     """
     Calculate average precision and precision-recall breakeven.
 
@@ -32,8 +34,12 @@ def average_precision(pos_distances, neg_distances, show_plot):
     using the true positive distances `pos_distances` and true negative
     distances `neg_distances`.
     """
-    distances = np.concatenate([pos_distances, neg_distances])
-    matches = np.concatenate([np.ones(len(pos_distances)), np.zeros(len(neg_distances))])
+    distances = np.concatenate(
+        [pos_distances, neg_distances]
+        )
+    matches = np.concatenate(
+        [np.ones(len(pos_distances)), np.zeros(len(neg_distances))]
+        )
 
     # Sort from shortest to longest distance
     sorted_i = np.argsort(distances)
@@ -68,6 +74,38 @@ def average_precision(pos_distances, neg_distances, show_plot):
     return average_precision, prb
 
 
+def mean_average_precision(distances, labels):
+    """
+    Calculate mean average precision and precision-recall breakeven.
+
+    Returns
+    -------
+    mean_average_precision, mean_prb, ap_dict : float, float, dict
+        The dict gives the per-type average precisions.
+    """
+
+    label_matches = generate_matches_array(labels)  # across all tokens
+
+    ap_dict = {}
+    prbs = []
+    for target_type in sorted(set(labels)):
+        if len(np.where(np.asarray(labels) == target_type)[0]) == 1:
+            continue
+        type_matches = generate_type_matches_array(labels, target_type)
+        swtt_matches = np.bitwise_and(
+            label_matches == True, type_matches == True
+            ) # same word, target type
+        dwtt_matches = np.bitwise_and(
+            label_matches == False, type_matches == True
+            ) # different word, target type
+        ap, prb = average_precision(
+            distances[swtt_matches], distances[dwtt_matches]
+            )
+        prbs.append(prb)
+        ap_dict[target_type] = ap
+    return np.mean(ap_dict.values()), np.mean(prbs), ap_dict
+
+
 def generate_matches_array(labels):
     """
     Return an array of bool in the same order as the distances from
@@ -81,7 +119,33 @@ def generate_matches_array(labels):
     cur_matches_i = 0
     for n in range(N):
         cur_label = labels[n]
-        matches[cur_matches_i:cur_matches_i + (N - n) - 1] = np.asarray(labels[n + 1:]) == cur_label
+        matches[cur_matches_i:cur_matches_i + (N - n) - 1] = np.asarray(
+            labels[n + 1:]
+            ) == cur_label
+        cur_matches_i += N - n - 1
+
+    return matches
+
+
+def generate_type_matches_array(labels, target_type):
+    """
+    Return an array of bool in the same order as the distances from
+    `scipy.spatial.distance.pdist` indicating whether a distance is between a
+    pair involving `target_type`.
+    """
+    N = len(labels)
+    matches = np.zeros(N*(N - 1)/2, dtype=np.bool)
+
+    # For every distance, mark whether it is a true match or not
+    cur_matches_i = 0
+    for n in range(N):
+        cur_label = labels[n]
+        if cur_label == target_type:
+            matches[cur_matches_i:cur_matches_i + (N - n) - 1] = True
+        else:
+            matches[cur_matches_i:cur_matches_i + (N - n) - 1] = np.asarray(
+                labels[n + 1:]
+                ) == target_type
         cur_matches_i += N - n - 1
 
     return matches
@@ -97,7 +161,9 @@ def fixed_dim(X, labels, metric="cosine", show_plot=False):
     N, D = X.shape
     matches = generate_matches_array(labels)
     distances = pdist(X, metric)
-    return average_precision(distances[matches == True], distances[matches == False], show_plot)
+    return average_precision(
+        distances[matches == True], distances[matches == False], show_plot
+        )
 
 
 #-----------------------------------------------------------------------------#
@@ -106,12 +172,14 @@ def fixed_dim(X, labels, metric="cosine", show_plot=False):
 
 def check_argv():
     """Check the command line arguments."""
-    parser = argparse.ArgumentParser(description=__doc__.strip().split("\n")[0], add_help=False)
+    parser = argparse.ArgumentParser(
+        description=__doc__.strip().split("\n")[0], add_help=False
+        )
     parser.add_argument("labels_fn", help="file of labels")
     parser.add_argument(
         "distances_fn",
-        help="file providing the distances between each pair of labels in the same order as "
-        "`scipy.spatial.distance.pdist`"
+        help="file providing the distances between each pair of labels in the "
+        "same order as `scipy.spatial.distance.pdist`"
         )
     parser.add_argument(
         "--binary_dists", dest="binary_dists", action="store_true",
@@ -138,32 +206,27 @@ def main():
     N = len(labels)
 
     # Read distances
-    print "Start time: " + str(datetime.datetime.now())
+    print("Start time: " + str(datetime.now()))
     if args.binary_dists:
-        print "Reading distances from binary file:", args.distances_fn
+        print("Reading distances from binary file:", args.distances_fn)
         distances_vec = np.fromfile(args.distances_fn, dtype=np.float32)
     else:
-        print "Reading distances from text file:", args.distances_fn
-        distances_vec = np.fromfile(args.distances_fn, dtype=np.float32, sep="\n")
+        print("Reading distances from text file:", args.distances_fn)
+        distances_vec = np.fromfile(
+            args.distances_fn, dtype=np.float32, sep="\n"
+            )
     if np.isnan(np.sum(distances_vec)):
-        print "Warning: Distances contain nan"
-        # distances_vec = np.nan_to_num(distances_vec)
-        # distances_vec[np.where(np.isnan(distances_vec))] = np.inf
-
-    # print "Reading distances from:", args.distances_fn
-    # distances_vec = np.zeros(N*(N - 1)/2)
-    # i_dist_vec = 0
-    # for line in open(args.distances_fn):
-    #     distances_vec[i_dist_vec] = float(line.strip())
-    #     i_dist_vec += 1
+        print("Warning: Distances contain nan")
 
     # Calculate average precision
-    print "Calculating statistics."
+    print("Calculating statistics.")
     matches = generate_matches_array(labels)
-    ap, prb = average_precision(distances_vec[matches == True], distances_vec[matches == False], False)
-    print "Average precision:", ap
-    print "Precision-recall breakeven:", prb
-    print "End time: " + str(datetime.datetime.now())
+    ap, prb = average_precision(
+        distances_vec[matches == True], distances_vec[matches == False], False
+        )
+    print("Average precision:", ap)
+    print("Precision-recall breakeven:", prb)
+    print("End time: " + str(datetime.now()))
 
 
 if __name__ == "__main__":
